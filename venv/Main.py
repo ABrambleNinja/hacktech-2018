b33a911bab6133f06394c6ca2a509a8b91b7df1a
gabrielbessler <gabrielbessler@gmail.com>
2018-03-03 01:06:10 -0800

    Working Role Generator

--- venv/Main.py
+++ venv/Main.py
@@ -6,23 +6,40 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = debugging
-adjectives_list = []
-colors_list = []
-debugging = True
-role_list = []
+
+class Data:
+    def __init__(self):
+        self.debugging = True
+        app.config['DEBUG'] = self.debugging
+        self.adjectives_list = []
+        self.colors_list = []
+        self.locations_dict = {}
+
+    def load_json_adjectives(self):
+        json_data = open("info.json").read()
+        data = json.loads(json_data)
+        self.adjectives_list = data["adjectives"]
+        self.colors_list = data["colors"]
+
+    def load_json_roles(self):
+        '''
+        Gets all of the location and role data from the JSON file
+        '''
+        json_data = open("locations.json").read()
+        self.locations_dict = json.loads(json_data)
 
 
 class Game:
     ''' Each game will be an instance of this object '''
-    def __init__(self, num_people):
+    def __init__(self, num_people, fancy=True):
         ''' Initializer for a new game '''
         self.num_people = num_people
         self.current_players = 0
-        self.location = get_random_location()
+        self.location = get_location()
         self.available_roles = get_roles(location)
         self.player_dictionary = {player_id: role for player_id, role in
                                   zip(list(range(num_people)), roles)}
+        self.fancy = fancy
 
     def update_size(self, num_people):
         ''' Change the number of people currently in the game '''
@@ -42,34 +59,24 @@ def game():
     ''' '''
     pass
 
-
-def load_json_ajectives():
-    json_data = open("info.json").read()
-    data = json.loads(json_data)
-    adjectives_list = data["adjectives"]
-    colors_list = data["colors"]
-
-
-def load_json_roles():
-    '''
-    Gets all of the location and role data from the JSON file
-    '''
-    json_data = open(file_directory).read()
-    data = json.loads(json_data)
+@app.route('/role_test')
+def get_some_role():
+    return get_role(get_location())
 
 
 def get_location():
-    ''' '''
-    return "sample location"
+    ''' Returns a random location from the set of possible values '''
+    return random.choice(list(DATA.locations_dict.keys()))
 
 
-def get_role(fancy=True):
-    ''' '''
-    role = random.choice(role_list)
+def get_role(location, fancy=True):
+    ''' Given a location, returns one of the possible roles from the location.
+    If fancy is enabled, this will also choose some color and an adjective '''
+    role = random.choice(DATA.locations_dict[location])
     if (fancy):
-        color = random.choice(colors_list)
-        adjective = random.choice(adjectives_list)
-        return color + " " + adjective + " " + role
+        color = random.choice(DATA.colors_list)
+        adjective = random.choice(DATA.adjectives_list)
+        return color + " " + adjective[0].upper() + adjective[1:] + " " + role
     else:
         return role
 
@@ -77,11 +84,14 @@ def get_role(fancy=True):
 def testing():
     ''' A function used on startup if in debugging mode '''
     print("In debugging mode")
+    print(get_role(get_location(), True))
 
 if __name__ == "__main__":
-    load_json_ajectives()
-    load_json_roles()
-    if debugging:
+    DATA = Data()
+    DATA.load_json_adjectives()
+    DATA.load_json_roles()
+
+    if DATA.debugging:
         testing()
 
     app.run()
