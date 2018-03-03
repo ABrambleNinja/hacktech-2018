a35d5511283ffcc4e28e5f68aad4b4d85484cab3
gbessler <gabrielbessler@gmail.com>
2018-03-02 23:15:27 -0800

    Initial Flask Commit

--- venv/Main.py
+++ venv/Main.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import json
+import pprint
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     ''' Index of the Page '''
     return "Hello, World"
+
+
+def load_json_data():
+    '''
+    Gets all of the location and role data from the JSON file
+    '''
+    json_data=open(file_directory).read()
+    data = json.loads(json_data)
 
 
 def get_location():
