68706e032ac2deec99e9a131f1c18c08fa12029c
Ben <bbaral@g.hmc.edu>
2018-03-03 20:55:16 -0800

    Modernize UI
    * Fix sessions so they work properly
    
    * re-added gps and such
    
    * test
    
    * Refactoring
    
    * Nevermind, no Refactoring
    
    * Add gitignore (long overdue)
    
    * Re-fix sessions
    
    * Deep moss green flabby old person
    
    * Bug Fix
    
    * Release under MIT license; properly attribute borrowed data
    
    * Bug Fixes
    
    * Nice going Gabe
    
    * added user IDs
    
    * Integrate GPS maybe
    
    * Add MAX_SIZE
    
    * Added distance function between GPS coordinates
    
    * added comments
    
    * fixed master
    
    * Clean up
    
    * fixed main

--- venv/Main.py
+++ venv/Main.py
@@ -2,26 +2,135 @@ import json
 import pprint
 import random
 import os
+import math
 
-from flask import Flask, session, render_template, request, url_for, abort
+from flask import Flask, session, render_template, request, url_for, abort, session
 
 app = Flask(__name__)
-playerList = {}
+
+@app.route('/')
+def index_page():
+    ''' Index of the Page '''
+    return render_template('index.html', default_people=DATA.DEFAULT_SIZE)
+
+
+@app.route('/checkGameOver', methods=["POST"])
+def check_game_over():
+    ''' Given a game ID, checks if the spy was discovered or not based on
+    the current. '''
+    game_id = request.get_json()
+    game = DATA.games[int(game_id)]
+    status = game.checkGameOver()
+    return json.dumps(status)
+
+@app.route('/newgame', methods=["POST"])
+def new_game():
+    ''' Creates a new game '''
+    # make new game
+
+    # L[0] = numPlayers
+    # L[1] = isFancy
+    L = request.get_json()
+    isFancy = L[1]
+    try:
+        num_players = int(L[0])
+        assert(num_players >= 1)
+    except:
+        return json.dumps(["error", "That is not a valid number of users."])
+
+    location_type = L[2]
+
+    time_limit = L[3]
+    if time_limit == "":
+        return json.dumps(["error", "You must put in a time limit."])
+    elif time_limit < 0:
+        return json.dumps(["error", "Time must be positive."])
+
+    if (len(DATA.games) < DATA.GAME_CAP):
+        if num_players > DATA.MAX_SIZE:
+            num_players = DATA.MAX_SIZE
+        # Initialize the game
+        newGame = Game(num_players, isFancy, location_type,
+                       time_limit, get_location())
+        # Find new game location (available ID)
+        indexValue = DATA.next_game_id()
+
+        # Add the game in the right ID location
+        if indexValue is None:
+            # If indexValue is None, we must add more slots
+            DATA.games.append(newGame)
+            indexValue = len(DATA.games) - 1
+        else:
+            # Otherwise, we can add it to the regular location
+            DATA.games[indexValue] = newGame
+
+        # Return the ID of the game to the player (to get URL)
+        return json.dumps(["ok", indexValue])
+    else:
+        return json.dumps(["error", "Cannot Create More Games"])
+
+
+@app.route('/game/<id_num>')
+def join_game(id_num):
+    ''' Connects user to existing game - if possible '''
+    print(id_num)
+    try:
+        id_num = int(id_num)
+    except:
+        abort(404, description="Not a Valid Game ID")
+
+    if (id_num >= len(DATA.games) or (DATA.games[id_num] is None)):
+        abort(404, description="Not a Valid Game")
+    else:
+        game = DATA.games[int(id_num)]
+        role, location = game.join_game(int(id_num))
+
+    return render_template("game.html", gameID=id_num,
+                           numPlayers=game.current_players,
+                           role=role,
+                           maxPlayers=game.num_people,
+                           location=location,
+                           time=game.time_limit)
+
+@app.errorhandler(404)
+def page_not_found(description):
+    return render_template('404.html', desc=description)
+
+
+def get_role(location, fancy=True):
+    ''' Given a location, returns one of the possible roles from the location.
+    If fancy is enabled, this will also choose some color and an adjective '''
+    role = random.choice(DATA.locations_dict[location])
+    if (fancy):
+        color = random.choice(DATA.colors_list)
+        adjective = random.choice(DATA.adjectives_list)
+        return (role, color + " " + adjective[0].upper() + adjective[1:] + " " + role)
+    else:
+        return role
+
+
+def get_location():
+        ''' Returns the user's location from the set of possible values '''
+        return random.choice(list(DATA.locations_dict.keys()))
+
 
 class Data:
     def __init__(self):
         ''' Initializes all of the data storage objects for the
         flask application '''
-        app.secret_key = os.urandom(24)
         self.GAME_CAP = 10
         self.DEFAULT_SIZE = 5
+        self.MAX_SIZE = 100
 
-        self.debugging = True
-        app.config['DEBUG'] = self.debugging
         self.adjectives_list = []
         self.colors_list = []
         self.locations_dict = {}
+        self.distanceMatrix = []
+        self.numPlayers = 0
         self.games = []
+        self.playerList = {}
+        self.playerID = 0
+        self.MAX_PLAYER_ID = 10000
 
     def load_json_adjectives(self):
         ''' Loads all of the adjectives and colors from the external info.JSON
@@ -38,21 +147,28 @@ class Data:
         json_data = open("locations.json").read()
         self.locations_dict = json.loads(json_data)
 
+    def next_game_id(self):
+        ''' Gets the index of the game array to insert the new game,
+         or returns None if it should be appended '''
+        for i in range(len(self.games)):
+            if self.games[i] == None:
+                return i
+        return None
 
 class Game:
     ''' Each game will be an instance of this object '''
-    def __init__(self, num_people, fancy=True, location_type="random", time_limit=0):
+    def __init__(self, num_people, fancy=True,
+                 location_type="random", time_limit=0, location=None):
         ''' Initializer for a new game '''
         self.num_people = num_people
         self.current_players = 0
-        if location_type == "random":
-            self.location = get_location()
+        self.location = location
         # When the game starts, all of the roles are available
         self.available_roles = DATA.locations_dict[self.location]
         self.player_dictionary = {}
         self.fancy = fancy
-        # TODO: add support for more than one spy, and allow user to put in how many spies
-        # they would like to have
+        # TODO: add support for more than one spy, and allow user to put
+        #  in how many spies they would like to have
         self.num_spies = 0
         self.num_max_spies = 1
         self.spy_id = -1
@@ -72,7 +188,7 @@ class Game:
     def join_game(self, gameid):
         ''' Allows a new person to join the game '''
         if 'gameid' in session and session['gameid'] == gameid: # person is already here
-            return (self.player_dictionary[session['userid']], self.location)
+            return (self.player_dictionary[session['userid']][0], self.location)
 
         session['gameid'] = gameid
         session['userid'] = self.current_players
@@ -81,14 +197,14 @@ class Game:
         # people in the game, and then give them a role that is available
         if self.current_players < self.num_people:
             if self.current_players == self.spy_id:
-                self.player_dictionary[self.current_players] = "Spy"
+                self.player_dictionary[self.current_players] = ["Spy", None]
                 self.current_players += 1
                 return ("Spy", "UNKNOWN")
             else:
                 role = self.available_roles[self.current_players]
                 if (self.fancy):
                     role = self.fancify_role(role)
-                self.player_dictionary[self.current_players] = role
+                self.player_dictionary[self.current_players] = [role, None]
                 self.current_players += 1
                 return (role, self.location)
         else:
@@ -100,7 +216,7 @@ class Game:
         adjective = random.choice(DATA.adjectives_list)
         return color + " " + adjective[0].upper() + adjective[1:] + " " + role
 
-    def set_custom_location(location, roles):
+    def set_custom_location(self, location, roles):
         ''' Allows the player to put in a custom location with associated
         roles.
         @params:
@@ -108,115 +224,99 @@ class Game:
         roles - List'''
         pass
 
-    def make_guess():
+    def make_guess(self, layer_guessing, guess):
         '''
         Allows a single user to make a guess for who they think the
         spy is.
         '''
-        pass
+        self.player_dictionary[player_guessing][1] = guess
 
-    def game_over():
+    def checkGameOver(self):
         '''
         Checks if the game is over
         '''
-        pass
+        timesAccused = [0 for i in range(self.num_people)]
 
-    @staticmethod
-    def next_game_id():
-        ''' Gets the index of the game array to insert the new game,
-         or returns None if it should be appended '''
-        for i in range(len(DATA.games)):
-            if DATA.games[i] == None:
-                return i
-        return None
-
-
-@app.route('/')
-def hello_world():
-    ''' Index of the Page '''
-    return render_template('index.html' )
-
-def index_page():
-    ''' Renders the home page of the website '''
-    return render_template('index.html', default_people=DATA.DEFAULT_SIZE)
-
-
-@app.route('/newgame', methods=["POST"])
-def new_game():
-    ''' Creates a new game '''
-    # make new game
-    L = request.get_json()
-    isFancy = L[1]
-    try:
-        num_players = int(L[0])
-    except:
-        num_players = DATA.DEFAULT_SIZE
-
-    location_type = L[2]
-    time_limit = L[3]
-
-    if (len(DATA.games) < DATA.GAME_CAP):
-        # Initialize the game
-        newGame = Game(num_players, isFancy, location_type, time_limit)
-        # Find new game location (available ID)
-        indexValue = Game.next_game_id()
-
-        # Add the game in the right ID location
-        if indexValue is None:
-            # If indexValue is None, we must add more slots
-            DATA.games.append(newGame)
-            indexValue = len(DATA.games) - 1
-        else:
-            # Otherwise, we can add it to the regular location
-            DATA.games[indexValue] = newGame
-
-        # Return the ID of the game to the player (to get URL)
-        return json.dumps(indexValue)
-    else:
-        return json.dumps("Cannot Create More Games")
-
-
-@app.route('/game/<int:id_num>')
-def join_game(id_num):
-    ''' Connects user to existing game - if possible '''
-    if (id_num >= len(DATA.games) or (DATA.games[id_num] is None)):
-        abort(404, description="Not a Valid Game")
-    else:
-        game = DATA.games[int(id_num)]
-        role, location = game.join_game(int(id_num))
-
-    return render_template("game.html", gameID=id_num,
-                           numPlayers=game.current_players,
-                           role=role,
-                           maxPlayers=game.num_people,
-                           location=location,
-                           time=game.time_limit)
-
+        print(self.player_dictionary)
+        for player_id, player_tuple in self.player_dictionary:
+            timesAccused[player_tuple[1]] += 1
 
 @app.route('/gpsdata', methods=["POST"])
 def gpsdata():
-    ''' Gets GPS coordinates from the user, appends to the list'''
+    ''' Gets GPS coordinates from the user, assigns an ID, and 
+    appends to the list'''
     data = request.data
-    print(data)
-    return json.dumps("this is the return thingy")
+    playerID = getID()
+    DATA.playerList[playerID] = {data}
+    print(playerID)
+    session['playerID'] = playerID 
+    #newDistEntry = updatePlayerMatrix(playerID)
+    #DATA.distanceMatrix.append(newDistEntry)
+    return "Player is in the database"
 
-@app.route('/userID', methods=["POST"])
-def iddata():
-    ''' Gets ID from the user'''
-    data = request.data
-    print(data)
-    return json.dumps("this is the test")
+def getID():
+    '''Takes the data variable playerID, assigns it to a user, and
+       then increments by 1 to give the player a unique id'''
+    playerID = DATA.playerID
+    if 'playerID' in session:
+        return (session['playerID'])
+    DATA.playerID += 1
+    updateNumPlayers()
+    return playerID
 
-def addGPS():
-    ''' Adds the user's location to the database of lists '''
-    return playerList
-    pass
+def updateNumPlayers():
+    DATA.numPlayers = len(DATA.playerList.keys())
+    return
 
-def calculateDist(userloc,x):
-    '''Finds all the locations in the list that are within x miles of user'''
+''' def updatePlayerMatrix(playerID):
+    keyList = list(DATA.playerList.keys())
+    print(keyList)
+    if len(keyList) == 1:
+        keyList = []
+    else:
+        keyList.remove(playerID)
+    print(keyList)
+    if len(keyList) <= 0:
+        return
+    newPlayerMatrix = []
+    for i in range (DATA.numPlayers):
+        print(i)
+        newPlayerMatrix[i] = calculateDist(playerID,keyList[i])
+    return newPlayerMatrix
+ '''
+def calculateDist(userID1,userID2):
+    '''Finds all the locations in the list that are within x miles of
+     user. Uses the formula from '''
+    # the data associated with a user ID is a weird set, this extracts
+    # the actual coordinates
+    csv1 = str(DATA.playerList[userID1])[3:-2]
+    csv2 = str(DATA.playerList[userID2])[3:-2]
+    gps1 = csv1.split(',')
+    gps2 = csv2.split(',')
+    gps1 = list(map(float,gps1))
+    gps2 = list(map(float,gps2))
+    lat1 = gps1[0]
+    lon1 = gps1[1]
+    lat2 = gps2[0]
+    lon2 = gps2[1]
+    distance = distFromLats(lat1,lon1,lat2,lon2)
+    print(distance)
+    return distance
 
-    pass
+def degreesToRadians(degrees):
+    '''degrees --> radians '''
+    return degrees * 3.14159265 / 180
 
+def distFromLats(lat1,lon1,lat2,lon2):
+    '''formula for calculating distance between spherical coordinates, outputs distance'''
+    earthRadiusKm = 6371
+    dLat = degreesToRadians(lat2-lat1)
+    dLon = degreesToRadians(lon2-lon1)
+    lat1 = degreesToRadians(lat1)
+    lat2 = degreesToRadians(lat2)
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2) 
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    return earthRadiusKm * c
 
 @app.errorhandler(404)
 def page_not_found(description):
@@ -249,12 +349,16 @@ def testing():
     print("In debugging mode")
     print(get_role(get_location(), True))
 
+
+
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    debugging = True
+    app.config['DEBUG'] = debugging
+
     DATA = Data()
     DATA.load_json_adjectives()
     DATA.load_json_roles()
-
-    if DATA.debugging:
-        testing()
+    DATA.debugging = debugging
 
     app.run(host='0.0.0.0')
